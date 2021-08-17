6ea6646dddfa7714830f5408dfbcfa6c9fb4fadf
SofiaMejia <A01252540@itesm.mx>
2021-08-17 01:08:10 +0000

    03 Perimetro

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    mat1 = float(input('Calificación de la materia: '))
+    mat2 = float(input('Calificación de la materia: '))
+    mat3 = float(input('Calificación de la materia: '))
+    mat4 = float(input('Calificación de la materia: '))
+    promedio = (mat1 + mat2 + mat3 + mat4) / 4
+    print('El promedio es: '+str(promedio))
+
 
 if __name__ == '__main__':
     main()
